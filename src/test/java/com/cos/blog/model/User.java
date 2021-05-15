1b310ee757cdc4c3e7d742738fb903d96a3c241d
emibgo2 <emibgo2@naver.com>
2021-05-16 03:09:32 +0900

    Spring Boot Blog Project Start +4 Spring Boot Start +9 Blog Project D+4
    Table create

--- src/test/java/com/cos/blog/model/User.java
+++ src/test/java/com/cos/blog/model/User.java
@@ -1,5 +1,7 @@
 package com.cos.blog.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -12,7 +14,8 @@ public class User {
     @Id //Primary key
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 프로젝트에서 연결된 DB의 넘버링 전략을 따라간다.
     private int id;// 시퀀스, auto_increment
-    @Column(nullable = false, length = 20)
+
+    @Column(nullable = false, length = 30)
     private String username;// ID
 
     @Column(nullable = false, length = 100) // 해쉬 화를 위한 자리를 넉넉하게 배정
@@ -21,8 +24,11 @@ public class User {
     @Column(nullable = false, length = 50)
     private String email;
 
-    @ColumnDefault("'user'") // ' 를 양옆으로 적음으로써  문자열이라는것을 알려줌
-    private String role; // 정확하게는 Enum을 써야 좋다. ( 권한 배정 ) 도메인- 범위가 정해졌다.
+
+    @ColumnDefault("'user'")
+
+    private String role;
+
 
     @CreationTimestamp // 시간이 자동으로 입력
     private Timestamp createDate;
