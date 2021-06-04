7812f89627a63d69ffbd3cb62b7a14c495b40ff6
emibgo2 <emibgo2@naver.com>
2021-06-04 11:03:14 +0900

    Spring Boot Blog Project Start +7 Spring Boot Start +11
    Get Select By Id Function Add

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -1,17 +1,43 @@
 package com.cos.blog.test;
 
+import ch.qos.logback.classic.pattern.MessageConverter;
 import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.function.Supplier;
 
 @RestController
 
 public class DummyControllerTest {
     @Autowired
     private UserRepository userRepository;
+
+    //{id} 주소로 파라메터를 전달 받을 수 있음.
+    // http:// localhost:8000/blog/dummy/user/3
+    @GetMapping("/dummy/user/{id}")
+    public User detail(@PathVariable int id) {
+
+        User user=userRepository.findById(id).orElseThrow(new Supplier<IllegalStateException>() {
+            @Override
+            public IllegalStateException get() {
+                return new IllegalStateException("해당 유저는 없습니다."+id);
+            }
+        });
+//        요청: 웹브라우저
+//        user 객체 = 자바 오브젝트
+//        변환(웹 브라우저가 이해할 수 있는 데이터) -> json (Gson 라이브러리)
+//        스프링부트 = MessageConverter라는 애가 응답시에 자동 작동
+//        만약에 자바 오브젝트를 리턴하게되면 MessageConverter가 Jackson 라이브러리를 호출해서
+//        user 오브젝트를 json으로 변환해서 브라우저에게 던져준다
+        return user;
+
+    }
 
     @PostMapping("/dummy/join")
     public String join(User user){
