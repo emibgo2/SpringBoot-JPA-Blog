facc7f395b476122b1ba1aefe3f87a792ae51ef0
emibgo2 <emibgo2@naver.com>
2021-06-06 01:00:19 +0900

    Spring Boot Blog Project Start +9 Spring Boot Start +13
    Add Update Function

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -9,11 +9,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.function.Supplier;
 
@@ -22,6 +20,28 @@ import java.util.function.Supplier;
 public class DummyControllerTest {
     @Autowired
     private UserRepository userRepository;
+
+    // save함수는 id를 전달하지 않으면 insert를 해주고
+    // save 함수는 id를 전달하면 해당 id에 대한 데이터가 있으면 update를해주고
+    // save 함수는 id를 전달하면 해당 id에 대한 데이터가 없으면 insert를 한다.
+    // email, password
+    @Transactional // update시 save 사용 안할수 잇도록 해주는 어노테이션
+    @PutMapping("/dummy/user/{id}")
+    public User updateUser(@PathVariable int id,@RequestBody User requestUser) {
+        System.out.println("id: "+id);
+        System.out.println("password: "+requestUser.getPassword());
+        System.out.println("email: "+ requestUser.getEmail());
+
+        User user = userRepository.findById(id).orElseThrow(()->{
+        return new IllegalArgumentException("수정에 실패하였습니다.");
+        });
+
+        user.setPassword(requestUser.getPassword());
+        user.setEmail(requestUser.getEmail());
+
+//        userRepository.save(user);
+        return null;
+    }
 
     // http://localhost:8000/blog/dummy/user/
     @GetMapping("/dummy/users")
