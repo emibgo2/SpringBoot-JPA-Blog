ba53190620543aefd23308a7a327d2d2fe1b094f
emibgo2 <emibgo2@naver.com>
2021-06-16 13:20:28 +0900

    Spring Boot Blog Project Start +19 Spring Boot Start +23
    Spring Security
    Sing up after Hashing Password

--- src/main/java/com/cos/blog/config/SecurityConfig.java
+++ src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.cos.blog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 // 빈 등록: 스프링 컨테이너에서 객체를 관리할 수 있게 하는 것
@@ -13,11 +15,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity(prePostEnabled = true) // 특정 주소로 접근하면 권한 및 인증을 미리 체크
 // 위 3개는 그냥 세트이기때문에 시큐리티 설정할때 그냥 걸면됨
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean // IoC, 해당 메소드를 스프링이 관리
+    public BCryptPasswordEncoder encodePWD() {
+
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable() // csrf 토큰 비활성화 (테스트시에만 걸어두는게 좋음)
                 .authorizeRequests()
-                .antMatchers("/auth/**")
+                .antMatchers("/","/auth/**","/js/**","/css/**","/image/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -6,6 +6,7 @@ import com.cos.blog.model.User;
 import com.cos.blog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,11 +19,12 @@ public class UserApiController {
     private UserService userService;
 
 
+
+
     @PostMapping("/auth/joinProc")
     public ResponseDto<Integer> save(@RequestBody User user) {
         System.out.println("UserApiController: save 호출됨");
         // 실제로 DB에  insert를 하고 아래에서 return 이 되면된다.
-        user.setRole(RoleType.USER);
         userService.회원가입(user);
         return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
     }
