4145e617d4f8335003fba100608d232d0807c752
emibgo2 <emibgo2@naver.com>
2021-06-16 12:58:45 +0900

    Spring Boot Blog Project Start +19 Spring Boot Start +23
    Spring Secuirty Login page customizing

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -18,7 +18,7 @@ public class UserApiController {
     private UserService userService;
 
 
-    @PostMapping("/api/user")
+    @PostMapping("/auth/joinProc")
     public ResponseDto<Integer> save(@RequestBody User user) {
         System.out.println("UserApiController: save 호출됨");
         // 실제로 DB에  insert를 하고 아래에서 return 이 되면된다.
@@ -27,14 +27,6 @@ public class UserApiController {
         return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
     }
 
-    @PostMapping("/api/user/login")
-    public ResponseDto<Integer> login(@RequestBody User user, HttpSession session) {
-        System.out.println("UserApiController: login 호출됨");
-        User principal = userService.로그인(user); // principal(접근 주체)
-        if (principal != null) {
-            session.setAttribute("principal",principal);
-        }
-        return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
-    }
+
 
 }
