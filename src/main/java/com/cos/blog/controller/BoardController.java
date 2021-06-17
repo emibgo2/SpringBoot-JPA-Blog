73fcb15c004c73d20618964ab729449ec6e9bc69
emibgo2 <emibgo2@naver.com>
2021-06-17 13:52:16 +0900

    Spring Boot Blog Project Start +20 Spring Boot Start +24
    Spring Security Login system aply

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,6 +1,8 @@
 package com.cos.blog.controller;
 
 
+import com.cos.blog.config.auth.PrincipalDetail;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,8 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class BoardController {
 
     @GetMapping({"","/"})
-    public String index() {
+    public String index(@AuthenticationPrincipal PrincipalDetail principal) {
         // /WEB-INF/views/joinForm.jsp
+        System.out.println("로그인 사용자 아이디:"+ principal.getUsername());
         return "index";
     }
 }
--- src/main/java/com/cos/blog/repository/UserRepository.java
+++ src/main/java/com/cos/blog/repository/UserRepository.java
@@ -4,12 +4,17 @@ import com.cos.blog.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 
 // JSP로 치면 DAO
 // 자동으로 bean 들고이 된다.
 // @Repository 생략 가능
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+    // SELECT * FROM user WHERE username = 1?;
+    // findBy = SELECT * FROM user <- Optinal findBy/Username -> WHERE username
+    Optional<User> findByUsername(String username);
 }
 //JPA Naming 전략
 // SELECT * FROM user WHERE username= ? AND password = ?;
