82efe3fb834e70ae3aaf04f5a6bbb656bf44cbcc
emibgo2 <emibgo2@naver.com>
2021-06-23 19:07:59 +0900

    Spring Boot Blog Project Start +25 Spring Boot Start +29
    Create User Information Modify Function - 2 ( Session Modify ) / Modify Function Complete

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -1,11 +1,18 @@
 package com.cos.blog.controller.api;
 
+import com.cos.blog.config.auth.PrincipalDetail;
 import com.cos.blog.dto.ResponseDto;
 import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,6 +25,8 @@ import javax.servlet.http.HttpSession;
 public class UserApiController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
 
     @PostMapping("/auth/joinProc")
@@ -31,6 +40,11 @@ public class UserApiController {
     @PutMapping("/user")
     public ResponseDto<Integer> update(@RequestBody User user) { // 제이슨 데이터로 받고싶으면 RequestBody 써야함
         userService.회원수정(user);
+        // 여기서 트랜잭션이 종료되기 때문에 DB에 값은 변경되지만
+        // 세션값은 변경되지 않은 상태
+        // 세션 등록
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
         return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
     }
 }
