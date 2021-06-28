0bd4d6b99b8ad4e4f2d40dd3793e29b89a14aa1e
emibgo2 <emibgo2@naver.com>
2021-06-28 14:44:56 +0900

    Spring Boot Blog Project Start +30 Spring Boot Start +34
    Add comment deletion function

--- src/main/java/com/cos/blog/controller/api/BoardApiController.java
+++ src/main/java/com/cos/blog/controller/api/BoardApiController.java
@@ -21,7 +21,7 @@ public class BoardApiController {
 
     @PostMapping("/api/board")
     public ResponseDto<Integer> save(@RequestBody Board board, @AuthenticationPrincipal PrincipalDetail principal) {
-        boardService.글쓰기(board,principal.getUser());
+        boardService.글쓰기(board, principal.getUser());
         return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
     }
 
@@ -31,6 +31,7 @@ public class BoardApiController {
         return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 
     }
+
     @PutMapping("/api/board/{id}")
     public ResponseDto<Integer> update(@PathVariable int id, @RequestBody Board board) {
         boardService.글수정하기(id, board);
@@ -39,10 +40,15 @@ public class BoardApiController {
 
     //데이터 받을때 컨트롤러에서 DTO를 만들어서 받는게 좋다.
     @PostMapping("/api/board/{boardId}/reply")
-    public ResponseDto<Integer> replySave(@RequestBody ReplySaveRequestDto replySaveRequestDto ) {
+    public ResponseDto<Integer> replySave(@RequestBody ReplySaveRequestDto replySaveRequestDto) {
 
         boardService.댓글쓰기(replySaveRequestDto);
         return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
     }
 
+    @DeleteMapping("/api/board/{boardId}/reply/{replyId}")
+    public ResponseDto<Integer> replyDelete(@PathVariable int replyId) {
+        boardService.댓글삭제(replyId);
+        return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+    }
 }
--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -103,6 +103,20 @@ let index= {
             alert(JSON.stringify(error));
         });
     },
+    replyDelete: function (boardId,replyId) {
+        $.ajax({
+            type: "DELETE",
+            url: `/api/board/${boardId}/reply/${replyId}`,
+            dataType: "json" // 요청을 서버로해서 응답이 왔을때 기본적으로 모든것이 문자열로오는데
+            // 생긴게 json이라면 => javascript 오브젝트로 변경 해줌
+        }).done(function (resp) {
+            alert("댓글삭제가 완료되었습니다.");
+            // console.log(resp)
+            location.href = `/board/${boardId}`;
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
 }
 
 index.init()
